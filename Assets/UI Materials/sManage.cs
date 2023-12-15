a9f0134d535ff2f3af66952658603090e0ab34ad
houch520 <chino081324@gmail.com>
2023-12-16 01:34:03 +0800

    UI sample

---
+++ Assets/UI Materials/sManage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sManage : MonoBehaviour
+{
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Continue()
+    {
+
+    }
+    public void Tutorial()
+    {
+
+    }
+    public void Training()
+    {
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
