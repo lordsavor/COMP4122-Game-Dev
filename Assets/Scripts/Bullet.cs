e916683a64195c1af28d36dd053342a841e19f0b
lordsavor <63599521+lordsavor@users.noreply.github.com>
2023-12-17 16:07:31 +0800

    Simple firing
    just hold lmb to shoot
    will imrpove

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletGravityScale = 0.2f; // Adjust this value to change the gravity scale
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = true; // Enable gravity for the bullet
+
+        // Destroy the bullet and its GameObject after 5 seconds
+        Destroy(gameObject, 5f);
+    }
+
+    void FixedUpdate()
+    {
+        // Modify gravity scale indirectly by adjusting Physics.gravity
+        Physics.gravity = new Vector3(0, -9.81f * bulletGravityScale, 0);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
