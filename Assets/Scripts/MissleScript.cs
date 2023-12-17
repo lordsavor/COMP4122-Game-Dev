f91f2d740bbdd3799c754c9ef4ed1e37bd054adf
houch520 <chino081324@gmail.com>
2023-12-17 23:56:11 +0800

    added homing missile
    missile target should be set to a object with rigidbody component

---
+++ Assets/Scripts/MissleScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleScript : MonoBehaviour
+{
+    public Transform missleSpawnPoint;
+    public GameObject misslePrefab;
+    public float roundsPerMinute = 600;
+    public GameObject target;
+    private float timeBetweenShots;
+    private bool canFire = true;
+
+    void Start()
+    {
+        timeBetweenShots = 600f / roundsPerMinute;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1) && canFire)
+        {
+            StartCoroutine(Firemissles(target));
+        }
+    }
+
+    IEnumerator Firemissles(GameObject targetObject)
+    {
+        canFire = false;
+
+        GameObject missile = Instantiate(misslePrefab, missleSpawnPoint.position, missleSpawnPoint.rotation);
+        Tarodev.Missile missileScript = missile.AddComponent<Tarodev.Missile>();
+        if (missileScript != null)
+        {
+            missileScript.ChangeTarget(targetObject);
+            missileScript.SetIsCloned(true);
+            missileScript.ChangeRB(missile.GetComponent<Rigidbody>());
+        }
+
+        yield return new WaitForSeconds(timeBetweenShots);
+
+        canFire = true;
+    }
+}
