c2cb1e4e25965bfe08462406b7bd6d3d791eb6f5
houch520 <chino081324@gmail.com>
2023-12-18 19:00:50 +0800

    Ground Enemy AI+shoting

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Moving
+    [SerializeField] public int ShipBaseSpeed = 100;
+
+    // Shooting
+    [SerializeField] public Transform bulletSpawnPoint;
+    [SerializeField] public GameObject bulletPrefab;
+    [SerializeField] public float bulletBaseSpeed = 2000;
+    [SerializeField] public LayerMask whatIsGround, whatIsPlayer;
+    [SerializeField] public Transform Checkpoint;
+
+    // Attacking
+    public Transform player;
+    private float timeBetweenShots;
+    private bool canFire = true;
+
+    // State
+    bool attackState = false;
+    public float sightRange, attackRange;
+    [HideInInspector] public bool playerInSightRange, playerInAttackRange;
+
+
+    void Start()
+    {
+        // Look at checkpoint and adjust rotation
+        Vector3 targetDirection = (Checkpoint.position - transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up) * Quaternion.Euler(0f, 90f, 0f);
+        transform.rotation = targetRotation;
+
+        GetComponent<Rigidbody>().velocity = targetDirection * ShipBaseSpeed;
+    }
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (playerInSightRange)
+        {
+            // Assign the detected player's transform to the player variable
+            Collider[] players = Physics.OverlapSphere(transform.position, sightRange, whatIsPlayer);
+            if (players.Length > 0)
+            {
+                player = players[0].transform;
+            }
+        }
+
+        if (playerInAttackRange)
+        {
+            attackState = true;
+            Attack();
+        }
+        else
+        {
+            attackState = false;
+        }
+    }
+
+    private void Attack()
+    {
+        if (canFire&& attackState)
+        {
+            StartCoroutine(FireBullets());
+        }
+    }
+
+    IEnumerator FireBullets()
+    {
+        canFire = false;
+
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+
+        // Get the direction to the target position
+        Vector3 targetDirection = (transform.position - bulletSpawnPoint.position).normalized;
+
+        bullet.GetComponent<Rigidbody>().velocity = targetDirection * bulletBaseSpeed;
+
+        // Destroy the bullet after 5 seconds (adjust as needed)
+        Destroy(bullet, 5f);
+
+        yield return new WaitForSeconds(timeBetweenShots);
+
+        canFire = true;
+    }
+}
---
+++ Assets/Scripts/EnemySpawning.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour
+{
+
+    [SerializeField] public GameObject Enemy;
+    [SerializeField] public int MaximumEnemy = 10;
+
+    public int enemyCount;
+    private int BasexPos;
+    private int BasezPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BasexPos = (int)transform.position.x;
+        BasezPos = (int)transform.position.z;
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (enemyCount < MaximumEnemy)
+        {
+            int xPos = BasexPos + Random.Range(1, 50);
+            int zPos = BasezPos + Random.Range(1, 50);
+            Instantiate(Enemy, new Vector3(xPos, transform.position.y, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount++;
+        }
+    }
+}
