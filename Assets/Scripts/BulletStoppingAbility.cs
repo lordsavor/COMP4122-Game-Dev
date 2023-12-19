1be30e45a924fda451a90d3133ebb59306473014
houch520 <chino081324@gmail.com>
2023-12-19 19:44:59 +0800

    bullet stop without effect

---
+++ Assets/Scripts/BulletStoppingAbility.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStoppingAbility : MonoBehaviour
+{
+    [SerializeField] public LayerMask bulletLayer;
+    [SerializeField] public float stoppingRadius = 50f;
+    [SerializeField] public float destroyInterval = 0.1f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            StopBullets();
+        }
+    }
+
+    private void StopBullets()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, stoppingRadius, bulletLayer);
+
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody bulletRb = collider.GetComponent<Rigidbody>();
+            if (bulletRb != null)
+            {
+                bulletRb.isKinematic = true;
+                collider.enabled = false;
+                StartCoroutine(DestroyBulletCoroutine(collider.gameObject));
+            }
+        }
+    }
+
+    private IEnumerator DestroyBulletCoroutine(GameObject bullet)
+    {
+        yield return new WaitForSeconds(destroyInterval);
+        Destroy(bullet);
+    }
+}
