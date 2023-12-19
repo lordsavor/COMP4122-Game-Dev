4ac5e02fc2840d84e6297b64c11fb15cec7dc12d
houch520 <chino081324@gmail.com>
2023-12-19 22:51:04 +0800

    deadExplosion update
    Explosion effect for enemy death also

---
+++ Assets/Scripts/DeadExplosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadExplosion : MonoBehaviour
+{
+    public ParticleSystem destroyEffect;
+    public float destroyDuration = 2f;
+
+    private void OnDestroy()
+    {
+        ParticleSystem effect = Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(effect.gameObject, destroyDuration);
+    }
+}
