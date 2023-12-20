29dede6c4efb54c67ea45094ca2bd5bf1c0e8555
houch520 <chino081324@gmail.com>
2023-12-20 19:11:17 +0800

    dmg updated

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -7,21 +7,15 @@ public class EnemyDamage : MonoBehaviour
     public int maxHits = 10; // Set the maximum number of hits required to make the enemy disappear
     private int currentHits = 0;
 
-    void FixedUpdate()
+    private void OnTriggerEnter(Collider other)
     {
-        // debug
-        // Debug.Log("Enemy Hit! Current Hits: " + currentHits);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        // Check if the colliding object is a bullet (you can customize the tag or layer)
-        if (collision.gameObject.CompareTag("Bullet"))
+        // Check if the triggering object is a bullet (you can customize the tag or layer)
+        if (other.CompareTag("Bullet"))
         {
             // Increase the hit count
             currentHits++;
 
-            Destroy(collision.gameObject);
+            Destroy(other.gameObject);
 
             // Check if the maximum hits have been reached
             if (currentHits >= maxHits)
